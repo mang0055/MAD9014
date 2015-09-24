712319ee9cefd5b5e025dbf262b0183f46e7496d
Raviraj Mangukiya <mang0055@algonquinlive.com>
2015-09-24 11:59:33 -0400

    Assignment Done. Way 1.

--- Week-3-Assignment-4/js/ex.js
+++ Week-3-Assignment-4/js/ex.js
@@ -1,3 +1,32 @@
 // JavaScript Document
+var status_text;
+var enablePopup=false;
+var howerMainDiv;
+function load() {
+    //console.log("load event detected!");
+	status_text=document.getElementById("status");
+	howerMainDiv=document.getElementById("hower-main");
+	howerMainDiv.onmouseover = function() {
+    	//console.log("Mouse over");
+    	enablePopup=true;
+    	status_text.innerHTML="Mouse Over";
+	}
+	
+	howerMainDiv.onmouseout = function() {
+    	//console.log("Mouse is out");
+    	status_text.innerHTML="Mouse is Outside";
+	}
+	
+	status_text.addEventListener("click", function()
+		{
+			if(enablePopup){
+				//console.log("Clicked");
+				alert('Click!');
+			}
+			
+		});
+}
+window.onload = load;
+
 
 
